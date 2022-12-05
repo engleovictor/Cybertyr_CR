09a4f226813902c2dc703addb36917e3502f4ad7
unknown <leotpw9@gmail.com>
2022-12-05 11:40:16 -0300

    add rsa3

--- Infra/main.tf
+++ Infra/main.tf
@@ -9,6 +9,6 @@ terraform {
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIASMPHVMNYTTU4NKEB"
-  secret_key  = "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"
+  #access_key = 
+  #secret_key  = 
 }
